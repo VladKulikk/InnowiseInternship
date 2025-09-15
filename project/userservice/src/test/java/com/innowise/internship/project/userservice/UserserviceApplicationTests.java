bdaa8725755bdae819a8848a5e94eeac490eb005
VladKulikk <kulikv2470@gmail.com>
2025-09-15 16:13:34 +0300

    added files from spring initializer repo

---
+++ project/userservice/src/test/java/com/innowise/internship/project/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.internship.project.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
