b277ab1f2be29f6c8e09da76f33051b2cb958e3b
VladKulikk <kulikv2470@gmail.com>
2025-09-29 15:37:11 +0300

    initial commit

---
+++ project/authentificationservice/src/main/java/com/innowise/internship/authentificationservice/AuthentificationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.internship.authentificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationserviceApplication.class, args);
+	}
+
+}
